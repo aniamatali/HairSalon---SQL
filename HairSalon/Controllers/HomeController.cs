ecf848e474a4afe3c3fa094e0d06c0a1c2470eab
Alvin Niamatali <aniamatali@icloud.com>
2017-10-27 10:06:03 -0700

    adding clients to employees working

--- HairSalon/Controllers/HomeController.cs
+++ HairSalon/Controllers/HomeController.cs
@@ -37,7 +37,23 @@ namespace HairSalon.Controllers
         model.Add("stylist", selectedStylist);
         model.Add("client", stylistClients);
         return View(model);
+      }
 
+      [HttpPost("/Stylists/{id}")]
+      public ActionResult AddAClient(int id)
+      {
+        string clientName = Request.Form["inputClient"];
+        Client newClient = new Client(clientName,id,(Request.Form["inputHours"]));
+        newClient.Save();
+
+        Dictionary<string, object> model = new Dictionary<string, object>();
+        Stylist selectedStylist = Stylist.Find(Int32.Parse(Request.Form["stylist-id"]));
+        List<Client> stylistClients = selectedStylist.GetClients();
+        model.Add("client", stylistClients);
+        model.Add("stylist", selectedStylist);
+
+
+        return View("ClientListForStylist", model);
       }
 
     }
